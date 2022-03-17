6e581148293a414c5dd4ed40629b67d9cb9feec5
Stoff <101693409+Stoff42069@users.noreply.github.com>
2022-03-17 00:29:26 -0400

    Add files via upload

--- Euler 4 fuck.py
+++ Euler 4 fuck.py
@@ -1,8 +1,11 @@
 def is_palindrome(num):
     return str(num) == str(num)[::-1]
 def largest(bot, top):
+    z = 0
     for x in range(top, bot, -1):
-        for y in range(top,bot, -1):
-            if is_palindrome(x*y):
-                return x*y
+        for y in range(top, bot, -1):
+            if is_palindrome(x * y):
+                if x * y > z:
+                    z = x * y
+    return z
 print(largest(100,999))
